5881cc78e24c010aed232e6cd32340074416cf31
Druzzdil <staniszewski86@gmail.com>
2017-06-27 10:01:31 +0200

    3

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
-
+const bootstrap = require("express-bootstrap-service");
 let app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.use('/assets/', express.static('assets'));
+app.use('/public/', express.static('public'));
+app.use(bootstrap.serve);
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
@@ -21,7 +23,7 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
+    welcomeMessage: 'Welcome to my website',
   });
 });
 
@@ -34,6 +36,4 @@ app.get('/about', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
-
-
 });
